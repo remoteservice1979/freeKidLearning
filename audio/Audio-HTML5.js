7e02182f541efd15c35811d47c98ae2cb8b63fd1
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2020-09-27 23:05:09 +0530

    Update Audio-HTML5.js
    file part

--- audio/Audio-HTML5.js
+++ audio/Audio-HTML5.js
@@ -3,8 +3,8 @@
 // PLAYER VARIABLES
 
 
-var mp3snd 	= "./audio-file.mp3";	// MP3 FILE
-var oggsnd 	= "./audio-file.ogg";	// OGG FILE
+var mp3snd 	= "./audio/audio-file.mp3";	// MP3 FILE
+var oggsnd 	= "./audio//audio-file.ogg";	// OGG FILE
 
 
 var loopsong	= "no"			// LOOP MUSIC | yes | no |
