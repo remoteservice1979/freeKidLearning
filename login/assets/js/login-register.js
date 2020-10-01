9718bfda03f36a259f82f670f37c95d9992ebe86
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2020-10-01 21:55:43 +0530

    Update login-register.js
    logincss

--- login/assets/js/login-register.js
+++ login/assets/js/login-register.js
@@ -1,11 +1,4 @@
-/*
- *
- * login-register modal
- * Autor: Creative Tim
- * Web-autor: creative.tim
- * Web script: http://creative-tim.com
- * 
- */
+
 function showRegisterForm(){
     $('.loginBox').fadeOut('fast',function(){
         $('.registerBox').fadeIn('fast');
@@ -13,47 +6,79 @@ function showRegisterForm(){
             $('.register-footer').fadeIn('fast');
         });
         $('.modal-title').html('Register with');
-    }); 
+    });
     $('.error').removeClass('alert alert-danger').html('');
-       
+
 }
 function showLoginForm(){
     $('#loginModal .registerBox').fadeOut('fast',function(){
         $('.loginBox').fadeIn('fast');
         $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');    
+            $('.login-footer').fadeIn('fast');
         });
-        
+
         $('.modal-title').html('Login with');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
+    });
+     $('.error').removeClass('alert alert-danger').html('');
 }
 
 function openLoginModal(){
     showLoginForm();
     setTimeout(function(){
-        $('#loginModal').modal('show');    
+        $('#loginModal').modal('show');
     }, 230);
-    
+
 }
 function openRegisterModal(){
     showRegisterForm();
     setTimeout(function(){
-        $('#loginModal').modal('show');    
+        $('#loginModal').modal('show');
     }, 230);
-    
+
 }
 
 function loginAjax(){
     /*   Remove this comments when moving to server
     $.post( "/login", function( data ) {
             if(data == 1){
-                window.location.replace("/home");            
+                window.location.replace("/home");
             } else {
-                 shakeModal(); 
+                 shakeModal();
             }
         });
     */
+    event.preventDefault();
+   $.ajax({
+       type: "GET",
+       datatype:"json",
+       url: "https://run.mocky.io/v3/3c33d063-99f1-4cf7-ad31-d5f11652f15c",
+       data:({
+           email : $('#email').val(),
+           password: $('#password').val()
+       }),
+       success: function(result)
+       {
+           if(result) // you should do your checking here
+           {
+             var email = $('#email').val();
+             var password = $('#password').val();
+             let data = result.filter(user => user.useremail === email && user.password === password );
+             if (data.length > 0 ){
+                window.location = 'http://www.google.com/';
+             }else{
+                window.location = 'http://www.yahoo.com/';
+             }
+
+               //just to show that it went through
+           }
+           else
+           {
+               $('#result').empty().addClass('error')
+                   .append('Something is wrong.');
+           }
+       }
+   });
+   return false;
 
 /*   Simulate error message from the server   */
      shakeModal();
@@ -63,9 +88,7 @@ function shakeModal(){
     $('#loginModal .modal-dialog').addClass('shake');
              $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
              $('input[type="password"]').val('');
-             setTimeout( function(){ 
-                $('#loginModal .modal-dialog').removeClass('shake'); 
-    }, 1000 ); 
+             setTimeout( function(){
+                $('#loginModal .modal-dialog').removeClass('shake');
+    }, 1000 );
 }
-
-   
