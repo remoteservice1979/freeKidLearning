39f0676020c4bf047d3c238bde8dd04468fbd346
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2020-10-04 12:17:12 +0530

    Update highscores.js
    add details

--- Study/Material/highscores.js
+++ Study/Material/highscores.js
@@ -1,8 +1,15 @@
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const currentUser = JSON.parse(localStorage.getItem("currentEmp")) || [];
 
 highScoresList.innerHTML = highScores
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
+
+studentList.innerHTML = currentUser
+  .map(user => {
+    return `<li class="high-score">${user.username} - ${user.useremail}</li>`;
   })
   .join("");
