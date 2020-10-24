ec37172e2bba3b09a0af382d561d47d68d4537d7
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2020-10-24 14:26:04 +0530

    Update game.js
    try

--- JobQuiz/QuizAppMaster/game.js
+++ JobQuiz/QuizAppMaster/game.js
@@ -15,10 +15,9 @@ let questions = [];
 var skill = localStorage.getItem("skill");
 fetch(
    
-     skill ==="DOT NET" ? 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c897' : 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c897';
+     skill ==="DOT NET" ? 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c897' : 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c822'
     
-         
-         
+              
      
 )
     .then((res) => {
