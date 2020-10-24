4bb7312e51eec63f226e96e23cf41f919c90888d
Anil Kumar <45123631+remoteservice1979@users.noreply.github.com>
2020-10-24 14:31:36 +0530

    Update game.js
    Doremon

--- JobQuiz/QuizAppMaster/game.js
+++ JobQuiz/QuizAppMaster/game.js
@@ -15,7 +15,7 @@ let questions = [];
 var skill = localStorage.getItem("skill");
 fetch(
    
-     skill ==="DOT NET" ? 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c897' : 'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-85ee-04e1b4d9c897'
+  'https://run.mocky.io/v3/7fae2ede-8cc5-4f90-85ee-04e1b4d9c897'
     
               
      
